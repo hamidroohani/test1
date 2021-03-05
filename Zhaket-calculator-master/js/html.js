61aca288426cd6e02fc08dcc870ef71990319f5c
HamidRoohani <hamid.dev@yahoo.com>
2021-03-05 20:30:43 +0330

    .

---
+++ Zhaket-calculator-master/js/html.js
@@ -0,0 +1,35 @@
+window.htmlToolBox = () => {
+    return `
+<div class="robot-binance-popup" id="robot-binance-popup"><img src="${chrome.extension.getURL('/badge.png')}" alt="open"></div>
+
+<div class="buy-panel">
+    <div class="first">خرید و فروش</div>
+    <div class="price-panel">
+        <div class="buy">
+            <span>قیمت خرید</span>
+            <span>۱۲۳</span>
+        </div>
+        <div class="now">
+            <span>قیمت اکنون</span>
+            <span>۴۵۶</span>
+        </div>
+        <div class="sell">
+            <span>قیمت فروش</span>
+            <span>۷۸۹</span>
+        </div>
+    </div>
+    <div class="buy-form">
+        <div class="price">
+            <span>درصد خرید از سرمایه</span>
+            <input type="number">
+        </div>
+        <div class="stop">
+            <span>درصد استاپ</span>
+            <input type="number">
+        </div>
+        <div class="submit"><span>خرید</span></div>
+     </div>
+</div>
+`
+
+};
---
+++ Zhaket-calculator-master/js/main.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", function () {
+
+    // insert html template
+
+    document.body.insertAdjacentHTML('beforeend', htmlToolBox());
+
+    // open box
+    document.querySelector('.buy-form .submit > span').addEventListener('click', async () => {
+        document.querySelector("#__APP > div > div > div.css-12zxtqt > div > div.css-1txw6ev > div.css-1gfgxlv > div > div > span").click();
+        document.querySelector("input[name='stopPrice']").value = '20';
+        document.querySelector("input[name='quantity']").value = '20';
+        document.querySelector('#orderformSellBtn').parentElement.closest("form").submit();
+    });
+    document.querySelector('.robot-binance-popup').addEventListener('click', async () => {
+
+        let date = prompt('لطفا سال و ماه دریافتی را به صورت زیر وارد کنید: \n 1399/11 \n 1399/11/28');
+
+        if (date === null)
+            return null;
+
+        if (!window.location.href.includes('https://www.binance.com/dashboard/transactions'))
+            return alert('این قابلیت فقط در بخش مالی > تراکنش ها در دسترس می باشد.');
+
+        if (document.querySelector('.robot-binance-popup').classList.value === 'robot-binance-popup loading')
+            return null;
+
+        document.querySelector('.robot-binance-popup').classList.add('loading');
+
+        await window.start_calculator_f(date);
+
+        document.querySelector('.robot-binance-popup').classList.remove('loading');
+
+    });
+
+
+    let myInterval = setInterval(()=>{
+        let buyPrice = document.querySelector('.orderbook-list:last-child .orderbook-progress:first-child .bid-light');
+        if (buyPrice)
+        {
+            buyPrice = buyPrice.textContent;
+            document.querySelector('.buy-panel > .price-panel > .buy > span:last-child').textContent = buyPrice;
+        }
+
+        let nowPrice = document.querySelector('.contractPrice');
+        if (nowPrice)
+        {
+            nowPrice = nowPrice.textContent;
+            document.querySelector('.buy-panel > .price-panel > .now > span:last-child').textContent = nowPrice;
+        }
+
+        let sellPrice = document.querySelector('.orderbook-list:first-child .orderbook-progress:last-child  .ask-light');
+        if (sellPrice)
+        {
+            sellPrice = sellPrice.textContent;
+            document.querySelector('.buy-panel > .price-panel > .sell > span:last-child').textContent = sellPrice;
+        }
+    },100)
+});
